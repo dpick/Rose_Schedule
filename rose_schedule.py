f9b8115623ae03bd0809693709b38bb8c281fb89
David Pick <david@david-laptop.(none)>
2010-08-11 09:58:53 -0500

    added an actual homepage

--- rose_schedule.py
+++ rose_schedule.py
@@ -4,11 +4,22 @@ from Cheetah.Template import Template
 from datetime import time, date, datetime
 
 class HomePage:
+  @cherrypy.expose
+  def index(self):
+    return '''
+    <a href="/nextperiod/">Time Till Next Period</a>
+
+    '''
   
+class TimeTillNextPeriod:
   @cherrypy.expose
   def index(self):
     tmpl = Template(file='rose_schedule_template.html')
     tmpl.period = return_difference()
     return str(tmpl)
 
-cherrypy.quickstart(HomePage())
+root = HomePage()
+root.nextperiod = TimeTillNextPeriod()
+cherrypy.tree.mount(root)
+
+cherrypy.quickstart()
