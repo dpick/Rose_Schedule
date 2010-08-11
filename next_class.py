cbba040d30f29b40896cf8d5d2ddbf492889c5bd
David Pick <pickdavid@gmail.com>
2010-08-10 19:45:32 -0500

    pulled code for next class into another file

--- next_class.py
+++ next_class.py
@@ -13,23 +13,24 @@ time(15, 20),
 time(16, 20)
 ]
 
-for i in range(len(schedule)):
-    schedule[i] = datetime.combine(datetime.now().date(), schedule[i])
-
-periods = ["1st", "2nd", "3rd", "4th", "5th",  "6th", "7th", "8th", "9th", "10th"]
-
 def current_period():
-    current = datetime.now()
-    for i in range(len(schedule)):
-        if schedule[i] > current:
-            return i
-    return 0
+  current = datetime.now()
+  for i in range(len(schedule)):
+      if schedule[i] > current:
+          return i
+  return 0
 
-current = current_period()
-if current == 9:
-    current = 0
+def return_difference():
+  for i in range(len(schedule)):
+      schedule[i] = datetime.combine(datetime.now().date(), schedule[i])
+
+  periods = ["1st", "2nd", "3rd", "4th", "5th",  "6th", "7th", "8th", "9th", "10th"]
+
+  current = current_period()
+  if current == 9:
+      current = 0
 
 
-difference = schedule[current] - datetime.now()
-difference = (':'.join(str(difference).split(':')[:2])).split(" ")[::-1]
-print difference[0] + " untill " + str(periods[current])
+  difference = schedule[current] - datetime.now()
+  difference = (':'.join(str(difference).split(':')[:2])).split(" ")[::-1]
+  return difference[0] + " untill " + str(periods[current])
--- rose_schedule.py
+++ rose_schedule.py
@@ -1,4 +1,5 @@
 import cherrypy
+import next_class
 from Cheetah.Template import Template
 from datetime import time, date, datetime
 
@@ -8,24 +9,8 @@ class HomePage:
   def index(self):
     tmpl = Template(file='rose_schedule_template.html')
     tmpl.period_phrase = 'It is currently: '
-    tmpl.period = self.get_period()
+    tmpl.period = '1st' #return_difference()
     return str(tmpl)
-
-  def get_period(self):
-    periods = [('1st', time( 8,  5)),
-               ('2nd', time( 9,  0)),
-               ('3rd', time( 9, 55)),
-               ('4th', time(10, 50)),
-               ('5th', time(11, 45)),
-               ('6th', time(12, 40)),
-               ('7th', time(13, 35)),
-               ('8th', time(14, 30)),
-               ('9th', time(16, 25)),
-               ('10th', time(17, 20))]
-
-    for period, period_time in periods:
-      if datetime.now().time() < period_time:
-        return period
 
 if __name__ == '__main__':
   cherrypy.root = HomePage()
